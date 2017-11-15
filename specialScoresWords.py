e5df68eb7b989441b739542762c033203eae2e3f
David <gelpiorama@gmail.com>
2017-11-15 17:47:24 +0100

    test words with bigger ssw

--- specialScoresWords.py
+++ specialScoresWords.py
@@ -2,7 +2,7 @@
 
 def find_word(num_let, max_ssw):
 
-    WORD_LIST = ["question", "security", "southern", "null"]
+    WORD_LIST = ["question", "security", "southern", "null",  "etadidnac", "candidate"]
 
     if num_let == 0 or max_ssw == 0:
         return None
@@ -19,12 +19,10 @@ def find_word(num_let, max_ssw):
         else:
             pass
 
-    # assert exactSswWords[0][1] == "southern", "lista mal formada"
-    assert (888, "question") in exactSswWords
-    assert (888, "security") in exactSswWords
-    assert (888, "southern") in exactSswWords
-
-    return exactSswWords.pop()[-1]
+    if exactSswWords:
+        return exactSswWords.pop()[-1]
+    else:
+        return None
 
 
 if __name__ == "__main__":
@@ -36,3 +34,11 @@ if __name__ == "__main__":
     num_let = 8
     max_ssw = 888
     assert find_word(num_let, max_ssw) == 'southern', "test southern"
+
+    num_let = 9
+    max_ssw = 500
+    assert find_word(num_let, max_ssw) is None
+
+    num_let = 9
+    max_ssw = 925
+    assert find_word(num_let, max_ssw) == "candidate"
