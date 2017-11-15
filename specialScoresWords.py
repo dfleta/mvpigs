90a274bd172ef8bd387a08bcd4f801aebe742f10
David <gelpiorama@gmail.com>
2017-11-15 18:11:50 +0100

    word with highest ssw but smaller than max_ssw

--- specialScoresWords.py
+++ specialScoresWords.py
@@ -2,25 +2,36 @@
 
 def find_word(num_let, max_ssw):
 
-    WORD_LIST = ["question", "security", "southern", "null",  "etadidnac", "candidate"]
+    WORD_LIST = ["question", "security", "southern", "null",
+                 "etadidnac", "candidate",
+                 "pasport", "support"]
 
     if num_let == 0 or max_ssw == 0:
         return None
 
     exactSswWords = []
+    highestSsw = ""
 
     for word in WORD_LIST:
+        if len(word) != num_let:
+            continue
+
         ssw = 0
         for letter in word:
             ssw = ssw + ord(letter)
 
         if ssw == max_ssw:
             exactSswWords.append((ssw, word))
+        elif not exactSswWords and ssw < max_ssw:
+            highestSsw = word
         else:
+            # ssw > max_ssw
             pass
 
     if exactSswWords:
         return exactSswWords.pop()[-1]
+    elif highestSsw:
+        return highestSsw
     else:
         return None
 
@@ -37,8 +48,12 @@ if __name__ == "__main__":
 
     num_let = 9
     max_ssw = 500
-    assert find_word(num_let, max_ssw) is None
+    assert find_word(num_let, max_ssw) is None, "test None"
 
     num_let = 9
     max_ssw = 925
     assert find_word(num_let, max_ssw) == "candidate"
+
+    num_let = 7
+    max_ssw = 1412
+    assert find_word(num_let, max_ssw) == "support"
