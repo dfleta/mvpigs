1394bd214233db6f7f00df4c9a05ec82bb68187c
David <gelpiorama@gmail.com>
2017-11-15 16:34:53 +0100

    test max_ssw 0

--- specialScoresWords.py
+++ specialScoresWords.py
@@ -2,10 +2,12 @@
 
 def find_word(num_let, max_ssw):
 
-    if num_let == 0:
+    if num_let == 0 or max_ssw == 0:
         return None
 
 
 if __name__ == "__main__":
 
     assert find_word(0, 888) is None
+
+    assert find_word(888, 0) is None
